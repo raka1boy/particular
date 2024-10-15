9b82d3b222187149f8c553144322c6bbd366b352
raka1boy <artemka2015kek@gmail.com>
2024-10-16 02:26:39 +0300

    got SDL working thanks to wonderful Zig people, castholm and unarei. Now only supporting windows

---
+++ src/main.zig
@@ -0,0 +1,16 @@
+//! By convention, main.zig is where your main function lives in the case that
+//! you are building an executable. If you are making a library, the convention
+//! is to delete this file and start with root.zig instead.
+const std = @import("std");
+const c = @cImport({
+    @cDefine("SDL_DISABLE_OLD_NAMES", {});
+    @cInclude("SDL3/SDL.h");
+    @cDefine("SDL_MAIN_HANDLED", {});
+    @cInclude("SDL3/SDL_main.h");
+});
+
+pub fn main() !void {
+    std.debug.print("{any}", .{c.SDL_GetVersion()});
+        //std.debug.print("{any}", .{ });
+    // ...
+}
